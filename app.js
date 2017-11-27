27e4f0c7ce4fa5e984b30279b7c12955d944d931
GAM <gam230596@gmail.com>
2017-11-27 22:04:31 +0700

    fix origin

--- app.js
+++ app.js
@@ -22,12 +22,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(function(req, res, next) {
-    var allowedOrigins = ['http://localhost:3000'];
+    var allowedOrigins = ['https://blockchain1412139.herokuapp.com'];
     var origin = req.headers.origin;
     if(allowedOrigins.indexOf(origin) > -1){
         res.setHeader('Access-Control-Allow-Origin', origin);
     }
-    //res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:3000');
+    //res.header('Access-Control-Allow-Origin', 'https://blockchain1412139.herokuapp.com');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.header('Access-Control-Allow-Credentials', true);
